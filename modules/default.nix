10eb3716a99f0c738f504f519a38858033c79348
matt1432 <matt@nelim.org>
2024-01-25 15:31:47 -0500

    feat: manage corosync from this module

--- modules/default.nix
+++ modules/default.nix
@@ -5,15 +5,35 @@
   self,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkOption types;
+  inherit (builtins) typeOf;
 
   pacemakerPath = "services/cluster/pacemaker/default.nix";
   cfg = config.services.pacemaker;
+  cfgCoro = config.services.corosync;
 in {
   disabledModules = [pacemakerPath];
   imports = ["${nixpkgs-pacemaker}/nixos/modules/${pacemakerPath}"];
 
+  options.services.pacemaker = {
+    clusterName = mkOption {
+      type = types.str;
+      default = cfgCoro.clusterName;
+    };
+
+    nodes = mkOption {
+      type = typeOf cfgCoro.nodelist;
+      default = cfgCoro.nodelist;
+    };
+  };
+
   config = mkIf cfg.enable {
+    services.corosync = {
+      enable = true;
+      clusterName = mkIf (cfg.clusterName != cfgCoro.clusterName) cfg.clusterName;
+      nodelist = mkIf (cfg.nodes != cfgCoro.nodelist) cfg.nodes;
+    };
+
     # FIXME: https://github.com/NixOS/nixpkgs/pull/208298
     nixpkgs.overlays = [self.overlays.default];
   };
