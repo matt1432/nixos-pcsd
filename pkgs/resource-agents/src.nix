31f77990170a03263bc755064329a50664fb4f28
Updater <robot@nowhere.invalid>
2024-07-18 07:05:20 +0000

    ci: resource-agents v4.14.0rc1 -> v4.15.0rc1

--- pkgs/resource-agents/src.nix
+++ pkgs/resource-agents/src.nix
@@ -2,6 +2,6 @@
 {
   owner = "ClusterLabs";
   repo = "resource-agents";
-  rev = "v4.14.0rc1";
-  hash = "sha256-Tbfkktjy5hJUBazit7/8G+V42uehbhKECfxlf0UFUm8=";
+  rev = "v4.15.0rc1";
+  hash = "sha256-Hc0PEuEVw0WmT4naoDa7XYRicPMcnFgBZviyebvrYAo=";
 }
