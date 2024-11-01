18409e63b0ecf9def30a2259d57efced2170922e
Updater <robot@nowhere.invalid>
2024-11-01 07:05:24 +0000

    ci: pacemaker Pacemaker-2.1.8 -> Pacemaker-2.1.9

--- pkgs/pacemaker/src.nix
+++ pkgs/pacemaker/src.nix
@@ -2,6 +2,6 @@
 {
   owner = "ClusterLabs";
   repo = "pacemaker";
-  rev = "Pacemaker-2.1.8";
-  hash = "sha256-J57+NSs4cL3BlEjSaXAoUTEhbMMxhWy8sUZ/K3x2nRM=";
+  rev = "Pacemaker-2.1.9";
+  hash = "sha256-L/LQS5XLps0pqTfMAh1ZiR00SVltrNxMl6DXQhXBw1Q=";
 }
