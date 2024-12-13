2632c3a670f73fada8e0830c3f6152524a17b323
Updater <robot@nowhere.invalid>
2024-12-13 07:05:28 +0000

    chore: update ruby deps

--- pkgs/pcs/gemset.nix
+++ pkgs/pcs/gemset.nix
@@ -66,10 +66,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "0q9jm4pzqxk92dq9a1y7gjwcw3dcsm1mnsdhi9ms5hw1dpnprzlx";
+      sha256 = "1574gi74z5pww36rv0jvqlv9ybm87h7c37fb5r2axn3mbh0wwcs5";
       type = "gem";
     };
-    version = "1.6.2";
+    version = "1.6.3";
   };
   mustermann = {
     dependencies = ["ruby2_keywords"];
